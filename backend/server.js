e55707f2db37d753bf7a3a0944267a53774f5ef2
Mohammad Samani <mosam102@uni-duesseldorf.de>
2024-11-21 17:56:00 +0100

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const corsOptions = { origin: ['http://localhost:3000'] }
+
+const app = express()
+const PORT = 5000
+
+app.use(cors(corsOptions))
+app.use(express.json())
+
+app.get('/api', (req, res) => {
+  res.json({ app: 'artgallery' })
+})
+
+app.listen(PORT, () => {
+  'server started on port 5000...'
+})
