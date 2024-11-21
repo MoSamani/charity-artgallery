c3b6462294818245690f41d4786ad84022e29d01
Mohammad Samani <mosam102@uni-duesseldorf.de>
2024-11-21 18:33:18 +0100

    Checked backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
+  const fetchAPI = async () => {
+    const response = await axios.get('http://localhost:5000/api')
+    console.log(response.data)
+  }
+  useEffect(() => {
+    fetchAPI()
+  }, [])
   return <div>App</div>
 }
 
