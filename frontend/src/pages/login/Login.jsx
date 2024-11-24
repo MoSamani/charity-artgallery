3ae5da5183a7da158f6d60cd65a75560e028962a
Mohammad Samani <mosam102@uni-duesseldorf.de>
2024-11-24 08:25:09 +0100

    Added Navbar to the pages

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Navbar from '../../components/Navbar'
 
 function Login() {
-  return <div>Login</div>
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
 }
 
 export default Login
