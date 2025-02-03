3eb338832c26832e72350527886c5bca528576cd
Mohammad Samani <mosam102@uni-duesseldorf.de>
2025-02-03 18:10:52 +0100

    Added mongoose CRUD functions for users in user controller

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,24 +1,64 @@
 const User = require('../models/user')
 
-const getAllUsers = (req, res) => {
-  res.send('get all user')
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({})
+    res.status(200).json({ users: users })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
-const getUser = (req, res) => {
-  res.json({ id: req.params.id })
+const getUser = async (req, res) => {
+  try {
+    const { id: userId } = req.params
+    const user = await User.findById(userId)
+    if (!user) {
+      res.status(404).json({ msg: `No User with id: ${userId}` })
+    }
+    res.status(200).json({ user: user })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 const createUser = async (req, res) => {
-  const user = await User.create(req.body)
-  res.status(201).json(user)
+  try {
+    await User.create(req.body)
+    res.status(201).json(user)
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
-const updateUser = (req, res) => {
-  res.json({ id: req.params.id })
+const updateUser = async (req, res) => {
+  try {
+    const { id: userId } = req.params
+    const user = await User.findByIdAndUpdate({ _id: userId }, req.body, {
+      new: true,
+      runValidators: true,
+    })
+    if (!user) {
+      res.status(404).json({ msg: `No User with id: ${userId}` })
+    }
+    res.status(200).json({ user: user })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
-const deleteUser = (req, res) => {
-  res.json({ id: req.params.id })
+const deleteUser = async (req, res) => {
+  try {
+    const { id: userId } = req.params
+    console.log(userId)
+    const user = await User.findOneAndDelete({ _id: userId })
+    if (!user) {
+      res.status(404).json({ msg: `No User with id: ${userId}` })
+    }
+    res.status(200).json({ user: user })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 module.exports = { getAllUsers, getUser, createUser, updateUser, deleteUser }
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,10 +1,25 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-  firstname: String,
-  lastname: String,
-  password: String,
-  email: String,
+  firstname: {
+    type: String,
+    required: [true, 'firstname must be provided'],
+    trim: true,
+  },
+  lastname: {
+    type: String,
+    required: [true, 'lastname must be provided'],
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'password must be provided'],
+    minlength: [8, 'Password should contains at least 8 characters'],
+  },
+  email: {
+    type: String,
+    required: true,
+  },
   favorites: [String],
 })
 
